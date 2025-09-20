2d7b0b465f281bd38420eac20bc9a5b2925bc69c
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-09-20 18:20:10 +0200

    Moved BookmarkComponentProp declaration and other things into common This is because I will create more BookmarkComponents.

--- src/components/common/bookmarks/Bookmark.tsx
+++ src/components/common/bookmarks/Bookmark.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import type { Bookmark } from '@/lib/bookmarks';
-
-type BookmarkComponentProps = {
-  bookmark: Bookmark;
-  onClick?: (bookmark: Bookmark) => void;
-};
-
-const onClickNotImplemented = (bookmark: Bookmark) => {
-  console.warn(`onClick not implemented for bookmark:`, bookmark);
-};
+import { type BookmarkComponentProps, onClickNotImplemented } from './common';
 
 export const BookmarkComponent: React.FC<BookmarkComponentProps> = ({ bookmark, onClick = onClickNotImplemented }) => {
   return (
---
+++ src/components/common/bookmarks/common.ts
@@ -0,0 +1,10 @@
+import type { Bookmark } from '@/lib/bookmarks';
+
+export type BookmarkComponentProps = {
+  bookmark: Bookmark;
+  onClick?: (bookmark: Bookmark) => void;
+};
+
+export const onClickNotImplemented = (bookmark: Bookmark) => {
+  console.warn(`onClick not implemented for bookmark:`, bookmark);
+};
