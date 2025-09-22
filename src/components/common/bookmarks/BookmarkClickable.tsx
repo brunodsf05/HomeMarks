5c8f6908f1e9abd24cae740ce1962f947c5d5feb
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-09-22 13:38:38 +0200

    Moved bookmarks styling to css module

--- src/components/common/bookmarks/BookmarkClickable.tsx
+++ src/components/common/bookmarks/BookmarkClickable.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Favicon } from '@/components/common/Favicon';
 import { type BookmarkComponentProps, onClickNotImplemented } from './common';
+import styles from './BookmarkClickable.module.css';
 
 export const BookmarkClickable: React.FC<BookmarkComponentProps> = ({ bookmark, onClick = onClickNotImplemented }) => {
-
   return (
-    <div onClick={() => onClick(bookmark)} style={{ display: 'inline-flex', alignItems: 'center', gap: '4px' }}>
+    <div onClick={() => onClick(bookmark)} className={styles.this} >
       {bookmark.url && <Favicon url={bookmark.url} size={32} />}
       <p>{bookmark.title}</p>
     </div>
