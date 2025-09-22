3831744b7ad7ccc48145a2c392cafea73a822046
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-09-22 11:25:34 +0200

    Created functions to generate urls to get favicons

---
+++ src/lib/favicon.ts
@@ -0,0 +1,26 @@
+// import browserApi from "./browser_api";
+
+export type Sizes = 8 | 16 | 32 | 64 | 128;
+type FaviconGetter = (url: string, size: Sizes) => string;
+
+/// --- UTILS ---
+const encode = encodeURIComponent;
+
+/// --- FAVICON GETTERS ---
+/**
+ * Generates URL that can be used to fetch favicons via Google's `t2.gstatic.com/faviconV2` service.
+ */
+export const getUrlFromT2GStatic: FaviconGetter = (url: string, size: Sizes) =>
+  `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${encode(url)}&size=${size}`;
+
+/**
+ * Generates URL that can be used to fetch favicons via an external service.
+ */
+export const getUrlOnline: FaviconGetter = (url: string, size: Sizes) =>
+  getUrlFromT2GStatic(url, size);
+
+/**
+ * Generates URL that can be used to fetch favicons locally without a connection.
+ */
+export const getUrlLocally: FaviconGetter = (url: string, size: Sizes) =>
+  `chrome://favicon/size/${size}/${encode(url)}`;
