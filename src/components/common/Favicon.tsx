379265af07326dd7cb8115aac0a842b7f9f75922
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-09-22 14:52:49 +0200

    Favicon can return the loaded image url

--- src/components/common/Favicon.tsx
+++ src/components/common/Favicon.tsx
@@ -4,23 +4,30 @@ import * as favicon from "@/lib/favicon";
 interface FaviconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   url: string;
   size: favicon.Sizes;
+  onImageSourceUpdate?: (src: string) => void;
 }
 
 /**
  * An image component that displays the favicon for a given URL.
- * 
+ *
  * @param url The URL of the website to fetch the favicon for.
  * @param size The size of the favicon to display (default is 16).
+ * @param onImageSourceUpdate Executed when the image loads, either successfully or when the fallback image is used.
  */
-export const Favicon: React.FC<FaviconProps> = ({ url, size, ...rest }) => {
+export const Favicon: React.FC<FaviconProps> = ({ url, size, onImageSourceUpdate, ...rest }) => {
   const [faviconUrl, setFaviconUrl] = useState<string>(favicon.getUrlOnline(url, size));
   const [isUsingFallback, setIsUsingFallback] = useState<boolean>(false);
 
   const handleLoad: React.ReactEventHandler<HTMLImageElement> = (event) => {
+    const img = event.currentTarget;
+
+    onImageSourceUpdate && onImageSourceUpdate(img.src);
+
+    // Handle fallback image
     if (isUsingFallback) return;
 
     // Image search failed
-    if (event.currentTarget.naturalWidth < size) {
+    if (img.naturalWidth < size) {
       setFaviconUrl('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII'); // TODO: Load/generate it from somewhere
       setIsUsingFallback(true);
     }
