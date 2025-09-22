e8851e52bda846883bc21d9dd3956374a49f4c00
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-09-22 13:06:54 +0200

    Test Favicon inside BookmarkClickable

--- src/components/common/bookmarks/BookmarkClickable.tsx
+++ src/components/common/bookmarks/BookmarkClickable.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Favicon } from '@/components/common/Favicon';
 import { type BookmarkComponentProps, onClickNotImplemented } from './common';
 
 export const BookmarkClickable: React.FC<BookmarkComponentProps> = ({ bookmark, onClick = onClickNotImplemented }) => {
+
   return (
-    <div onClick={() => onClick(bookmark)}>
-      {bookmark.title}
+    <div onClick={() => onClick(bookmark)} style={{ display: 'inline-flex', alignItems: 'center', gap: '4px' }}>
+      {bookmark.url && <Favicon url={bookmark.url} size={32} />}
+      <p>{bookmark.title}</p>
     </div>
   );
 };
