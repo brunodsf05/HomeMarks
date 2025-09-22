80b63433407d5171f21e7d6e6474cabce6c0f098
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-09-22 17:52:30 +0200

    Achieved desired looks for BookmarkClickable

--- src/components/common/bookmarks/BookmarkClickable.tsx
+++ src/components/common/bookmarks/BookmarkClickable.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import { Favicon } from '@/components/common/Favicon';
 import { type BookmarkComponentProps, onClickNotImplemented } from './common';
 import styles from './BookmarkClickable.module.css';
 
 export const BookmarkClickable: React.FC<BookmarkComponentProps> = ({ bookmark, onClick = onClickNotImplemented }) => {
+  const [faviconSrc, setFaviconSrc] = useState<string>(); // Just for the 
+
   return (
-    <div onClick={() => onClick(bookmark)} className={styles.this} >
-      {bookmark.url && <Favicon url={bookmark.url} size={64} className={styles.favicon} />}
+    <div onClick={() => onClick(bookmark)} className={styles.this}>
+      {bookmark.url && <div className={styles.faviconContainer}>
+        {faviconSrc && <div className={styles.faviconBackground} style={{ backgroundImage: `url(${faviconSrc})` }} />}
+        <Favicon
+          url={bookmark.url}
+          size={64}
+          className={styles.favicon}
+          onImageSourceUpdate={setFaviconSrc}
+        />
+      </div>}
+
       <p className={styles.title}>{bookmark.title}</p>
-    </div>
+    </div >
   );
 };
