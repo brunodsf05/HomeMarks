b827d5afd6a2c47dc0017de2d49fbf4319f4ae6d
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-09-23 16:19:53 +0200

    Achieved bookmark folder style with mockup

--- src/components/common/bookmarks/BookmarkClickable.tsx
+++ src/components/common/bookmarks/BookmarkClickable.tsx
@@ -1,22 +1,65 @@
 import { FaviconBox } from '@/components/common/favicon';
+import { BookmarkService } from '@/lib/bookmarks';
 import { type BookmarkComponentProps, onClickNotImplemented } from './common';
 import styles from './BookmarkClickable.module.css';
 
 export const BookmarkClickable: React.FC<BookmarkComponentProps> = ({ bookmark, onClick = onClickNotImplemented }) => {
+  const isFolder: boolean = BookmarkService.getInstance().isFolder(bookmark);
+
   return (
     <div onClick={() => onClick(bookmark)} className={styles.this}>
-      <FaviconBox
-        url={bookmark.url}
-        size={64}
-        draggable={false}
-        additionalClassNames={
-          {
-            container: styles.faviconContainer,
-            icon: styles.faviconIcon
-          }
-        }
-      />
+      {
+        isFolder
+          ? <div className={styles.faviconFolder}>
+            <FaviconBox
+              url="https://www.google.com"
+              size={32}
+              draggable={false}
+              additionalClassNames={{
+                container: styles.faviconContainerMultiple,
+                icon: styles.faviconIconMultiple
+              }}
+            />
+            <FaviconBox
+              url="https://www.instagram.com"
+              size={32}
+              draggable={false}
+              additionalClassNames={{
+                container: styles.faviconContainerMultiple,
+                icon: styles.faviconIconMultiple
+              }}
+            />
+            <FaviconBox
+              url="https://www.linkedin.com"
+              size={32}
+              draggable={false}
+              additionalClassNames={{
+                container: styles.faviconContainerMultiple,
+                icon: styles.faviconIconMultiple
+              }}
+            />
+            <FaviconBox
+              url="https://www.github.com"
+              size={32}
+              draggable={false}
+              additionalClassNames={{
+                container: styles.faviconContainerMultiple,
+                icon: styles.faviconIconMultiple
+              }}
+            />
+          </div>
+          : <FaviconBox
+            url={bookmark.url}
+            size={64}
+            draggable={false}
+            additionalClassNames={{
+              container: styles.faviconContainerSingle,
+              icon: styles.faviconIcon
+            }}
+          />
+      }
+
       <p className={styles.title}>{bookmark.title}</p>
-    </div >
+    </div>
   );
 };
