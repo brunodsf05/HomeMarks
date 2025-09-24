e8a2868a76db31712f267790afc5db5fe36ac63b
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-09-24 16:19:53 +0200

    BookmarkExplorer's PathBar can be easily scrolled with mouse wheel (vertical scroll event)

--- src/components/common/bookmarks/explorer/_PathBar.tsx
+++ src/components/common/bookmarks/explorer/_PathBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { BookmarkService, type Bookmark } from "@/lib/bookmarks";
 import { Leaf } from "./_PathLeaf";
 import styles from "./styles.module.css";
@@ -11,8 +12,24 @@ interface PathBarProps {
 export const PathBar: React.FC<PathBarProps> = ({ rootBookmark, currentBookmark, onFolderClick }) => {
   const path: Bookmark[] = BookmarkService.getInstance().getFolderRange(rootBookmark, currentBookmark);
 
+  // Converts vertical scrolling into horizontal 
+  const barRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const barEl = barRef.current;
+    if (!barEl) return;
+
+    const handleWheel = (e: WheelEvent) => {
+      e.preventDefault();
+      barEl.scrollLeft += e.deltaY;
+    };
+    barEl.addEventListener("wheel", handleWheel, { passive: false });
+
+    return () => barEl.removeEventListener("wheel", handleWheel);
+  }, []);
+
   return (
-    <div className={styles.path}>
+    <div ref={barRef} className={styles.path}>
       {
         path.map((b, idx) => (
           <Leaf key={idx} bookmark={b} onClick={onFolderClick} />
