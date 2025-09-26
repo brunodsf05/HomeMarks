f34c41501701778cd5e265f0b2caedd3dec0ee81
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-09-26 10:50:28 +0200

    Moved App to components

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/reset.css";
 import "@/global.css";
-import App from "@/App.tsx";
+import App from "@/components/app/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
