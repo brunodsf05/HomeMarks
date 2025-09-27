41eb7a178f2dc5bdece269fce581de99f42212fa
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-09-27 12:27:04 +0200

    Created DelayedInput component which allows debouncing

---
+++ src/components/app/_DelayedInput.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect, type InputHTMLAttributes } from "react";
+
+type onChange = (line: string) => void;
+
+interface DelayedInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  onInstantChange: onChange;
+  onDelayedChange: onChange;
+  delayChangeMs: number;
+}
+
+export const DelayedInput: React.FC<DelayedInputProps> = ({
+  onInstantChange,
+  onDelayedChange,
+  delayChangeMs,
+  ...inputProps
+}) => {
+  const [query, setQuery] = useState<string>("");
+
+  useEffect(() => {
+    onInstantChange(query);
+
+    const onDelayedChangeCaller = setTimeout(() => {
+      onDelayedChange(query);
+    }, delayChangeMs);
+
+    return () => clearTimeout(onDelayedChangeCaller);
+  }, [query]);
+
+  return (
+    <input
+      {...inputProps}
+      onChange={(e) => setQuery(e.target.value.trim())}
+    />
+  );
+};
