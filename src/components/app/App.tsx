dc4f283acea4b352b9894655e5e94788dc758fe0
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-10-10 11:02:41 +0200

    Refactor: Moved SearchResult outside of App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,59 +1,15 @@
 import { useState, useEffect, useRef } from "react";
+
 import { type Bookmark, BookmarkService } from "@/lib/bookmarks";
 import { WebSearchService } from "@/lib/websearch";
-import { initKeyNav } from "@/lib/keynav";
-import { BookmarkExplorer } from "@/components/common/bookmarks";
 import { exampleBookmark } from "@/globals.d"; // TODO: Remove
+
 import { DelayedInput } from "@/components/common/DelayedInput";
+
 import { KeyboardHints, type KeyboardHintsContext } from "./_KeyboardHints";
+import { SearchResult } from "./_SearchResults";
+
 import styles from "./App.module.css";
-
-interface SearchResultProps {
-  bookmarks?: Bookmark,
-  query: string;
-}
-
-const SearchResult: React.FC<SearchResultProps> = ({ bookmarks, query }) => {
-  const [explorers, setExplorers] = useState<Bookmark[]>([]);
-
-  useEffect(() => {
-    const clearKeyNav = initKeyNav();
-    return () => clearKeyNav();
-  });
-
-  // Filter
-  useEffect(() => {
-    if (bookmarks === undefined || query.length === 0) {
-      setExplorers([]);
-      return;
-    }
-
-    setExplorers([
-      BookmarkService.getInstance().filter(
-        bookmarks,
-        {
-          title: new RegExp(`${query}`, "i"),
-          type: "all",
-          style: "flat"
-        }
-      )
-    ]);
-  }, [bookmarks, query]);
-
-  // Render
-  if (!bookmarks) return <></>;
-
-  return (
-    <>
-      <BookmarkExplorer key={bookmarks.id} rootBookmark={bookmarks} />
-      {
-        explorers.map((b, i) =>
-          <BookmarkExplorer key={`${i}-${b.id}`} rootBookmark={b} />
-        )
-      }
-    </>
-  );
-};
 
 const App = () => {
   const [bookmarks, setBookmarks] = useState<Bookmark>();
---
+++ src/components/app/_SearchResults.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, useRef } from "react";
+import { type Bookmark, BookmarkService } from "@/lib/bookmarks";
+import { initKeyNav } from "@/lib/keynav";
+import { BookmarkExplorer } from "@/components/common/bookmarks";
+
+interface SearchResultProps {
+  bookmarks?: Bookmark,
+  query: string;
+}
+
+export const SearchResult: React.FC<SearchResultProps> = ({ bookmarks, query }) => {
+  const [explorers, setExplorers] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    const clearKeyNav = initKeyNav();
+    return () => clearKeyNav();
+  });
+
+  // Filter
+  useEffect(() => {
+    if (bookmarks === undefined || query.length === 0) {
+      setExplorers([]);
+      return;
+    }
+
+    setExplorers([
+      BookmarkService.getInstance().filter(
+        bookmarks,
+        {
+          title: new RegExp(`${query}`, "i"),
+          type: "all",
+          style: "flat"
+        }
+      )
+    ]);
+  }, [bookmarks, query]);
+
+  // Render
+  if (!bookmarks) return <></>;
+
+  return (
+    <>
+      <BookmarkExplorer key={bookmarks.id} rootBookmark={bookmarks} />
+      {
+        explorers.map((b, i) =>
+          <BookmarkExplorer key={`${i}-${b.id}`} rootBookmark={b} />
+        )
+      }
+    </>
+  );
+};
