12021137bdfacb5015d7371a4f0d3ff4e63d3aa7
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-01-16 10:10:13 +0000

    Docs: Added a reminded to later overhaul WebSearchService with something like StrategyPattern

--- src/lib/websearch/types.ts
+++ src/lib/websearch/types.ts
@@ -4,3 +4,9 @@
 export interface IWebSearchService {
   search(text: string): void;
 }
+
+/**
+ * TODO: Make the WebSearchService use something like the StrategyPattern.
+ *       In short, I want to have multiple search engines.
+ *       Example: The user's default search engine, youtube search, github...
+ */
